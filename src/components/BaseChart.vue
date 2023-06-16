e2bc8f891ad8200cf70245ed13d955c2d4bdc93e
ondrui <ondrui@yandex.ru>
2023-06-16 18:27:34 +0300

    -NEW- implemented simple logics load data from server

--- src/components/BaseChart.vue
+++ src/components/BaseChart.vue
@@ -21,6 +21,21 @@ export default {
         ]
       }
     };
+  },
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+      try {
+        const res = await fetch('http://localhost:3002/api');
+        const datasets = await res.json();
+        this.chartOptions.series[0].data = datasets.data.map(({ temp }) => temp);
+        console.log(datasets);
+      } catch (error) {
+        console.log('Error! Could not reach the server. ' + error);
+      }
+    }
   }
 };
 </script>
